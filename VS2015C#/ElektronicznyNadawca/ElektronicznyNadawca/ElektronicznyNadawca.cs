5d639a8430fa4b06384a534ef4e56c151db50781
rozkodowany <andyg75@gmail.com>
2016-08-12 10:49:29 +0200

    Przykładowy klient EN 7.3.0
    Example EN client for version 7.3.0 labs.wsdl

---
+++ VS2015C#/ElektronicznyNadawca/ElektronicznyNadawca/ElektronicznyNadawca.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronicznyNadawca
+{
+    class EN
+    {
+        String password;
+        String user;
+        Uri serviceUrl = new Uri("https://en-testwebapi.poczta-polska.pl/websrv/labs.php");
+        Uri wsdlUrl = new Uri("https://en-testwebapi.poczta-polska.pl/websrv/labs.wsdl");
+        ENLabs.ElektronicznyNadawca client = null;
+        List<ENLabs.przesylkaType> przesylki = new List<ENLabs.przesylkaType>();
+        public void setUserPassword(String user, String password)
+        {
+            this.password = password;
+            this.user = user;
+        }
+        public void setServiceAddress(Uri url)
+        {
+            serviceUrl = url;
+        }
+        public void setWSDLAddress(Uri url)
+        {
+            wsdlUrl = url;
+        }
+        private void setCredential()
+        {
+            checkCredential();
+            if (this.client == null)
+            {
+                this.client = new ENLabs.ElektronicznyNadawca();
+                System.Net.NetworkCredential credential = new System.Net.NetworkCredential();
+                credential.UserName = this.user;
+                credential.Password = this.password;
+                this.client.Url = serviceUrl.ToString();
+                System.Net.CredentialCache cc = new System.Net.CredentialCache();
+                cc.Add(new Uri(wsdlUrl.ToString()), "Basic", credential);
+                this.client.Credentials = cc;
+            }
+        }
+
+        private void checkCredential()
+        {
+            if (this.user == null || this.password == null)
+            {
+                throw new Exception("Nie ustawiono nazwy użytkownika i/lub hasła.");
+            }
+            if (this.serviceUrl == null || this.wsdlUrl == null)
+            {
+                throw new Exception("Nie ustawiono adresu webserwisu.");
+            }
+        }
+
+        public String hello(String parameter)
+        {
+            setCredential();
+            return this.client.hello(parameter);
+        }
+        public ENLabs.accountType[] getAccountList()
+        {
+            setCredential();
+            ENLabs.accountType[] accountList = this.client.getAccountList();
+            return accountList;
+        }
+        public ENLabs.profilType[] getUserProfileList(String userName)
+        {
+            setCredential();
+            ENLabs.accountType[] accountList = this.getAccountList();
+            foreach (ENLabs.accountType account in accountList)
+            {
+                if (account.userName.Equals(userName))
+                {
+                    return account.profil;
+                }
+            }
+            throw new Exception("Nie znaleziono użytkownika " + userName);
+        }
+        public ENLabs.profilType[] getCurrentUserProfileList()
+        {
+            return this.getUserProfileList(this.user);
+        }
+        public ENLabs.accountType getCurrentUserAccount()
+        {
+            setCredential();
+            foreach (ENLabs.accountType account in this.getAccountList())
+            {
+                if (account.userName.Equals(this.user))
+                {
+                    return account;
+                }
+            }
+            throw new Exception("Błąd pobierania listy użytkowników");
+        }
+        public ENLabs.addShipmentResponseItemType[] addShipment(List<ENLabs.przesylkaType> przesylki, int? idBufor = null)
+        {
+            setCredential();
+            if (idBufor.HasValue)
+                return this.client.addShipment(przesylki.ToArray(), idBufor.Value, true);
+            else
+                return this.client.addShipment(przesylki.ToArray(), 0, false);
+        }
+        public ENLabs.errorType[] createEnvelopeBufor(ENLabs.buforType bufor)
+        {
+            setCredential();
+            ENLabs.buforType[] b = { bufor };
+            ENLabs.errorType[] errors;
+            b = this.client.createEnvelopeBufor(b, out errors);
+            bufor.idBufor = b.First().idBufor;
+            foreach (ENLabs.errorType error in errors)
+            {
+                if (error.errorNumber != 0)
+                {
+                    throw new Exception("Nie udało się utowrznie Bufora z powodu błędów");
+                }
+            }
+            return errors;
+        }
+        public ENLabs.errorType[] setAktywnaKarta(int idKarta)
+        {
+            setCredential();
+            return this.client.setAktywnaKarta(idKarta);
+        }
+        public ENLabs.urzadNadaniaFullType[] getUrzedyNadania()
+        {
+            setCredential();
+            return client.getUrzedyNadania();
+        }
+        public byte[] getAddresLabelByGuidCompact(string[] guid, int idBufor, out ENLabs.errorType[] errors)
+        {
+            setCredential();
+            byte[] labels = this.client.getAddresLabelByGuidCompact(guid, idBufor, true, out errors);
+            foreach (ENLabs.errorType error in errors)
+            {
+                if (error.errorNumber != 0)
+                {
+                    throw new Exception("Nie udało się pobrać etykiet z powodu błędu");
+                }
+            }
+            return labels;
+        }
+    }
+}
---
+++ VS2015C#/ElektronicznyNadawca/ElektronicznyNadawca/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronicznyNadawca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EN client = new EN();
+            client.setServiceAddress(new Uri("https://en-testwebapi.poczta-polska.pl/websrv/labs.php"));
+            client.setWSDLAddress(new Uri("https://en-testwebapi.poczta-polska.pl/websrv/labs.wsdl"));
+            client.setUserPassword(null, null);
+
+
+            String retval = "";
+            ENLabs.errorType[] errors = null;
+            try
+            {
+                retval = client.hello("Andrzej");
+                System.Console.WriteLine(retval);
+                // -----------------------------------------------------------------------------
+                ENLabs.profilType[] profiles = pobierzProfileZalogowanegoUzytkownika(client);
+                ENLabs.kartaType[] karty = pobierzKartyZalogowanegoUzytkownika(client);
+                ENLabs.urzadNadaniaFullType[] urzedyNadania = pobierzUrzedyNadaniaDlaKarty(client, karty.First());
+                // -----------------------------------------------------------------------------
+                ustawBiezacaKarte(client, karty.First());
+                // -----------------------------------------------------------------------------
+                ENLabs.buforType bufor;
+                utworzNowyBufor(client, out errors, profiles, urzedyNadania, out bufor);
+                // -----------------------------------------------------------------------------
+                List<string> guidy = utworzPrzesylki(client, bufor);
+                // -----------------------------------------------------------------------------
+                errors = pobierzEtykietyZBuforu(client, bufor, guidy);
+
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                if (errors != null)
+                {
+                    foreach (ENLabs.errorType error in errors)
+                    {
+                        System.Console.WriteLine("[" + error.errorNumber + "] " + error.errorDesc);
+                    }
+                }
+
+            }
+            System.Console.WriteLine("KONIEC");
+            System.Console.ReadKey();
+        }
+
+        private static void utworzNowyBufor(EN client, out ENLabs.errorType[] errors, ENLabs.profilType[] profiles, ENLabs.urzadNadaniaFullType[] urzedyNadania, out ENLabs.buforType bufor)
+        {
+            bufor = zdefiniujBufor(profiles.First(), urzedyNadania.First());
+            errors = client.createEnvelopeBufor(bufor);
+            System.Console.WriteLine("Utworzno bufor " + bufor.idBufor);
+        }
+
+        private static List<string> utworzPrzesylki(EN client, ENLabs.buforType bufor)
+        {
+            System.Console.WriteLine("-----Utworzenie przesyłek");
+            List<ENLabs.przesylkaType> przesylki = new List<ENLabs.przesylkaType>();
+            przesylki.Add(createPocztexUiszczaOplateNadawca());
+            przesylki.Add(createPocztexBledna());
+            List<string> guidy = nadajPrzesylki(client, bufor, przesylki);
+            return guidy;
+        }
+
+        private static List<string> nadajPrzesylki(EN client, ENLabs.buforType bufor, List<ENLabs.przesylkaType> przesylki)
+        {
+            ENLabs.addShipmentResponseItemType[] przesylkiWyslane = client.addShipment(przesylki, bufor.idBufor);
+            List<String> guidy = new List<string>();
+            System.Console.WriteLine("-----Nadawanie przesyłek");
+            foreach (ENLabs.addShipmentResponseItemType przesylka in przesylkiWyslane)
+            {
+                if (przesylka.error == null)
+                {
+                    System.Console.WriteLine("[" + przesylka.numerNadania + "] : " + przesylka.guid);
+                    guidy.Add(przesylka.guid);
+                }
+                else
+                {
+                    System.Console.WriteLine("Przesyłka o guidzie " + przesylka.guid + " nie została nadana z powodu błędów");
+                    foreach (ENLabs.errorType error in przesylka.error)
+                    {
+                        System.Console.WriteLine("[" + error.errorNumber + "] " + error.errorDesc);
+                    }
+                }
+            }
+
+            return guidy;
+        }
+
+        private static ENLabs.errorType[] pobierzEtykietyZBuforu(EN client, ENLabs.buforType bufor, List<string> guidy)
+        {
+            System.Console.WriteLine("-----Pobranie etykiet");
+            ENLabs.errorType[] errors;
+            byte[] etykiety = client.getAddresLabelByGuidCompact(guidy.ToArray(), bufor.idBufor, out errors);
+            SaveData(@"etykiety-bufor-" + bufor.idBufor.ToString() + ".pdf", etykiety);
+            return errors;
+        }
+
+        private static ENLabs.urzadNadaniaFullType[] pobierzUrzedyNadaniaDlaKarty(EN c, ENLabs.kartaType karta)
+        {
+            ENLabs.urzadNadaniaFullType[] urzedyNadania = c.getUrzedyNadania();
+            foreach (ENLabs.urzadNadaniaFullType urzadNadania in urzedyNadania)
+            {
+                System.Console.WriteLine("Urząd nadania dla karty " + karta.idKarta + " to " + urzadNadania.urzadNadania);
+            }
+
+            return urzedyNadania;
+        }
+
+        private static void ustawBiezacaKarte(EN c, ENLabs.kartaType karta)
+        {
+            c.setAktywnaKarta(karta.idKarta);
+            System.Console.WriteLine("------Ustawiono bieżącą kartę na: " + karta.idKarta);
+        }
+
+        private static ENLabs.kartaType[] pobierzKartyZalogowanegoUzytkownika(EN c)
+        {
+            System.Console.Write("------Dostępne karty zalogowanego użytkownika:");
+            var karty = c.getCurrentUserAccount().karta;
+            foreach (ENLabs.kartaType karta in karty)
+            {
+                System.Console.Write(" " + karta.idKarta);
+            }
+            System.Console.WriteLine("");
+            return karty;
+        }
+
+        private static ENLabs.profilType[] pobierzProfileZalogowanegoUzytkownika(EN c)
+        {
+            ENLabs.profilType[] profiles = c.getCurrentUserProfileList();
+            System.Console.WriteLine("------Profile zalogowanego użytkownika");
+            foreach (ENLabs.profilType profile in profiles)
+            {
+                System.Console.WriteLine(profile.idProfil + " " + profile.nazwaSkrocona);
+            }
+
+            return profiles;
+        }
+
+        private static ENLabs.buforType zdefiniujBufor(ENLabs.profilType profil, ENLabs.urzadNadaniaFullType urzadNadania)
+        {
+            ENLabs.buforType bufor = new ENLabs.buforType();
+            bufor.dataNadania = DateTime.Now.AddDays(1);
+            bufor.profil = profil;
+            bufor.dataNadaniaSpecified = true;
+            bufor.opis = "Nowy bufor webapi " + DateTime.Now.AddDays(1).ToString();
+            bufor.urzadNadania = urzadNadania.urzadNadania;
+            bufor.urzadNadaniaSpecified = true;
+            return bufor;
+        }
+
+        public static ENLabs.przesylkaType createPocztexUiszczaOplateNadawca()
+        {
+            var pocztex = new ENLabs.uslugaKurierskaType();
+            pocztex.adres = new ENLabs.adresType();
+            pocztex.adres.nazwa = "Jan Kowalski";
+            pocztex.adres.miejscowosc = "Gdynia";
+            pocztex.adres.ulica = "10 Lutego";
+            pocztex.adres.kodPocztowy = "81-301";
+
+            pocztex.uiszczaOplate = ENLabs.uiszczaOplateType.NADAWCA;
+            pocztex.uiszczaOplateSpecified = true;
+            pocztex.termin = ENLabs.terminKurierskaType.EKSPRES24;
+            pocztex.terminSpecified = true;
+            var g = Guid.NewGuid();
+            pocztex.guid = g.ToString();
+            return pocztex;
+        }
+
+        public static ENLabs.przesylkaType createPocztexBledna()
+        {
+            var pocztex = new ENLabs.uslugaKurierskaType();
+            pocztex.adres = new ENLabs.adresType();
+            pocztex.adres.nazwa = "Jan Kowalski";
+            pocztex.adres.miejscowosc = "Gdynia";
+            pocztex.adres.ulica = "10 Lutego";
+            pocztex.adres.kodPocztowy = "81-301";
+
+            pocztex.uiszczaOplate = ENLabs.uiszczaOplateType.NADAWCA;
+            pocztex.uiszczaOplateSpecified = true;
+            pocztex.termin = ENLabs.terminKurierskaType.EKSPRES24;
+            pocztex.terminSpecified = true;
+            pocztex.guid = "123456";
+            return pocztex;
+        }
+        public static bool SaveData(string fileNamePath, byte[] Data)
+        {
+            BinaryWriter Writer = null;
+            try
+            {
+                Writer = new BinaryWriter(File.OpenWrite(fileNamePath));
+                Writer.Write(Data);
+                Writer.Flush();
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                Writer.Close();
+            }
+
+            return true;
+        }
+    }
+}
